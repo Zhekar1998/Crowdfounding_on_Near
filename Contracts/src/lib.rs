86aa4f29baa1c66842ab4612d71b8dbab02ed2e9
Zhekar1998 <evgeniy.lukatsky@gmail.com>
2022-04-25 13:10:35 +0300

    update

--- Contracts/src/lib.rs
+++ Contracts/src/lib.rs
@@ -1,4 +1,5 @@
 use near_contract_standards::non_fungible_token::metadata::TokenMetadata;
+use near_sdk::collections::LazyOption;
 use near_sdk::{borsh};
 use near_sdk::borsh::BorshDeserialize;
 use near_sdk::borsh::BorshSerialize;
@@ -11,7 +12,7 @@ use near_sdk::PanicOnDefault;
 use near_sdk::Promise;
 use near_sdk::collections::Vector;
 use serde_json::json;
-use near_sdk::serde::{Serialize, Deserialize};
+use near_sdk::serde::{Deserialize, Serialize};
 
 
 
@@ -29,7 +30,7 @@ pub trait ExtUser {
 
 #[near_bindgen]
 #[derive(
-Debug, BorshDeserialize, BorshSerialize, PanicOnDefault,
+BorshDeserialize, BorshSerialize, PanicOnDefault,
 )]
 
 pub struct DonationItem {
@@ -40,10 +41,22 @@ pub struct DonationItem {
     create_time: u64,
     type_found: bool,
     status: u8, //1-- base, 2-- premium, 3 association
-    data_path: String,
+    metadata: LazyOption<Metadata>,
     nft_data: Vec<TokenMetadata>,
     nft_price: Vec<u128>,
-    donate_base: Vec<DonateBase>
+    donate_base: Vec<DonateBase>,
+    active: bool
+}
+
+#[derive(Debug, Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Metadata {
+    name: String,
+    short_info: String,
+    image: Vec<String>,
+    video: String,
+    type_pro: bool, //true for commertial false for non-profit
+    project_tags: String,
 }
 
 #[derive(Debug, BorshDeserialize, BorshSerialize)]
@@ -54,16 +67,15 @@ pub struct DonateBase {
 }
 
 #[derive(
-    Debug, Serialize, Deserialize, BorshDeserialize, BorshSerialize, PanicOnDefault,
+    Debug, BorshDeserialize, BorshSerialize, PanicOnDefault,
 )]
-#[serde(crate = "near_sdk::serde")]
 pub struct DonationItemView {
     amount: u128,
     donated: u128,
     receiver: String,
     project_name: String,
     time_past: u64,
-   data_path: String,
+   metadata: Metadata,
     status: u8,
     nft_data: Vec<TokenMetadata>,
     nft_price: Vec<u128>
@@ -92,7 +104,7 @@ impl Donation {
         this
     }
     #[payable]
-    pub fn add_donation(&mut self, amount: u128, receiver: String, projectname: String, nftdata: Vec<TokenMetadata>, nftprice: Vec<u128>, type_found_param: bool, data_path_var: String ) -> bool {
+    pub fn add_donation(&mut self, amount: u128, receiver: String, projectname: String, nftdata: Vec<TokenMetadata>, nftprice: Vec<u128>, type_found_param: bool, metadata_var: Metadata ) -> bool {
         let status_var: u8;
         if env::attached_deposit()>=10u128.pow(24) {
             status_var = 2;
@@ -114,10 +126,11 @@ impl Donation {
             create_time: env::block_timestamp(),
             type_found: type_found_param,
             status: status_var,
-            data_path: data_path_var,
+            metadata: LazyOption::new(b"m", Some(&metadata_var)),
             nft_data: nftdata,
             nft_price: nftprice,
-            donate_base: Vec::new()
+            donate_base: Vec::new(),
+            active: true
         });
         true
     }
@@ -164,11 +177,13 @@ impl Donation {
                     Promise::new(AccountId::new_unchecked(String::from(ROYALTY_ID).clone())).transfer(el.donated*(ROYALTY));
                     Promise::new(AccountId::new_unchecked(String::from(CREATOR_ROYALTY_ID).clone())).transfer(el.donated*(CREATOR_ROYALTY));
                     Promise::new(AccountId::new_unchecked(el.receiver.clone())).transfer(el.donated*(100-ROYALTY-CREATOR_ROYALTY)/100);
+                    el.active = false;
                 }else {
                     if env::block_timestamp()-el.create_time >= 2592000000000000{
                         el.donate_base.iter().for_each(|don|{
                             Promise::new(don.donator_id.clone()).transfer(don.donate_near);
-                        })
+                        });
+                        el.active = false;
                     }
                 }
             }else{
@@ -177,10 +192,12 @@ impl Donation {
                        Promise::new(AccountId::new_unchecked(String::from(ROYALTY_ID).clone())).transfer(el.donated*(ROYALTY)/100);
                        Promise::new(AccountId::new_unchecked(String::from(CREATOR_ROYALTY_ID).clone())).transfer(el.donated*(CREATOR_ROYALTY)/100);
                        Promise::new(AccountId::new_unchecked(el.receiver.clone())).transfer(el.donated*(100-ROYALTY-CREATOR_ROYALTY)/100);
+                       el.active = false;
                    }else{
                        el.donate_base.iter().for_each(|don|{
                            Promise::new(don.donator_id.clone()).transfer(don.donate_near);
-                       })
+                       });
+                       el.active = false;
                    }
                }
             }
@@ -199,7 +216,7 @@ impl Donation {
                 project_name: el.project_name,
                 time_past: 2592000000000000-env::block_timestamp()-el.create_time,
                 status: el.status,
-                data_path: el.data_path,
+                metadata: el.metadata.get().unwrap(),
                 nft_data: el.nft_data,
                 nft_price: el.nft_price,
 
--- src/js/near-work.js
+++ src/js/near-work.js
@@ -5,7 +5,7 @@ import { Web3Storage } from 'web3.storage'
 import inspect from 'browser-util-inspect';
 
 import getConfig from "./config"
-import {utils} from "near-api-js";
+import { utils } from "near-api-js";
 
 
 
@@ -15,7 +15,7 @@ window.nearConfig = getConfig(process.env.NODE_ENV || "development");
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEM3MDQ0ZjcyRkExYTQzMmZiRjE1ZUE2MkIyNTc2MzlCRjQ0NUY5QUQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDE1NjM3Nzg1MzUsIm5hbWUiOiIxIn0.l33kUNtDs5irZj40uCh2WHoUgCl31T9WSbSSkbxV1_I";
 
 
-function makeStorageClient() {  return new Web3Storage({ token })}
+function makeStorageClient() { return new Web3Storage({ token }) }
 
 async function storeFiles(files) {
     const client = makeStorageClient();
@@ -64,20 +64,52 @@ async function doWork() {
 }
 
 var projects_array_lenght = 0
-async function update_project_list(){
+async function update_project_list() {
     let projects_array = [];
     projects_array = await get_projects();
-    if (projects_array_lenght !== projects_array.length){
+    if (projects_array_lenght !== projects_array.length) {
         projects_array_lenght = projects_array.length;
-
+        for (let i = 0; i < projects_array_lenght; i++) {
+            let img_link = [];
+            for (let y = 0; y < projects_array[i].metadata.image.length; y++) {
+                img_lin[y] = 'https://' + projects_array[i].metadata.image[y] + '.ipfs.dweb.link';
+            }
+            let project = project_list(projects_array[i].project_name, img_link, projects_array[i].metadata.short_info, projects_array[i].amount, projects_array[i].donated, projects_array[i].time_past, projects_array[i].metadata.type_pro, projects_array[i].active)
+            document.getElementById("projects").innerHTML += project;
+        }
     }
 
 }
 
-let timerID = setInterval(()=> update_project_list(), 30000);
+function project_list(name, img, shortinfo, ammount, money, time, type, status, active) {
+    let carusel = '';
+    let card_body = '';
+    let project_cart = '';
+    if (img.lenght > 1) {
+        carusel += '<div class="carousel-indicators"> <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={0} class="active" aria-current="true" aria-label="Slide 1"></button>';
+        for (let i = 1; i < img.lenght; i++) {
+            let b = i + 1;
+            carusel += '<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={' + i + '} aria-label="Slide ' + b + '"></button>';
+        }
+        carusel += '</div><div class="carousel-inner"><div class = "carousel-item active"><img src="' + img[0] + '" class = "d-block w-100" alt = "loadError" /><div style = {position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);}>' + name + '</div></div>';
+        for (let i = 1; i < img.lenght; i++) {
+            carusel += '<div class = "carousel-item"><img src="' + img[i] + '" class = "d-block w-100" alt = "loadError" /></div>';
+        }
+        carusel += '</div><button class = "carousel-control-prev" type = "button" data-bs-target = "#carouselExampleIndicators" data-bs-slide = "prev" > <span class = "carousel-control-prev-icon" aria-hidden = "true" > <span class = "visually-hidden" > Previous < /span> </span></button > <button class = "carousel-control-next" type = "button" data-bs-target = "#carouselExampleIndicators" data-bs-slide = "next"> <span class = "carousel-control-next-icon" aria-hidden = "true"><span class = "visually-hidden" > Next </span> </span></button></div>';
+    } else carusel += '<div><img src="' + img[0] + '" class = "d-block w-100" alt = "loadError" /><divstyle = {position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);}>' + name + '</div></div>';
+    project_cart = '<div class="col"><div class="card shadow-sm">' + carusel;
+    card_body += '<div class = "card-body" > <p class = "card-text" >' + shortinfo + '</p> <div class = "progress"><div class = "progress-bar" role = "progressbar" style = { width: "25%"} aria-valuemin = { 0 } aria-valuemax = {' + ammount + '} aria-valuenow = {' + money + '} </div> </div>'
+
+    card_body += '<div class = "d-flex justify-content-between align-items-center"> <div class = "btn-group"><button type = "button" class = "btn btn-sm btn-outline-secondary" > View </button> <button type = "button" class = "btn btn-sm btn-outline-secondary" > Donate </button> </div > <small class = "text-muted" >' + time + '</small> </div > </div> </div>'
+    project_cart += card_body + '</div>';
+    return project_cart;
+}
 
 
-function signedInFlow(){
+let timerID = setInterval(() => update_project_list(), 30000);
+
+
+function signedInFlow() {
 
     document.getElementById('sign-out').style.display = ' ';
     document.getElementById('account_id').style.display = '';
@@ -94,7 +126,7 @@ function signedInFlow(){
 }
 
 
-function signedOutFlow(){
+function signedOutFlow() {
     document.getElementById('sign-out').style.display = 'none';
     document.getElementById('account_id').style.display = 'none';
     document.getElementById('sign-in').style.display = ' ';
@@ -104,11 +136,12 @@ function signedOutFlow(){
             // The contract name that would be authorized to be called by the user's account.
             window.nearConfig.contractName
         );
-});
+    });
 }
 
 async function get_projects() {
     const response = await window.contract.get_donations();
+    response = JSON.parse(response);
     console.log(response);
     return response;
 }
@@ -116,40 +149,45 @@ async function get_projects() {
 
 
 var premium_payment = utils.format.parseNearAmount("20");
-async function add_donation(amount, type_found, name, file_cid){
+async function add_donation(amount, type_found, name, metadata, nft_data, nft_price) {
     console.log(inspect({
         "amount": amount,
         "receiver": window.accountId,
         "type_found_param": type_found,
-        "data_path_var": file_cid
+
 
     }));
     if (document.getElementById("premium_check").checked) {
         const account = await window.walletAccount.account();
         const balance = await account.getAccountBalance();
 
-        if( balance > premium_payment) {
-            await window.contract.add_donation(
-                {
+        if (balance > premium_payment) {
+            await window.contract.add_donation({
                     "amount": amount,
                     "receiver": window.accountId,
+                    "projectname": name,
+                    "nft_data": nft_data,
+                    "nft_price": nft_price,
                     "type_found_param": type_found,
-                    "data_path_var": file_cid
+                    "metadata": metadata
+
 
                 },
                 300000000000000,
                 premium_payment
             )
-        }else{
+        } else {
             //TODO errorState
         }
-    }else{
-        await window.contract.add_donation(
-            {
+    } else {
+        await window.contract.add_donation({
                 "amount": amount,
                 "receiver": window.accountId,
+                "projectname": name,
+                "nft_data": nft_data,
+                "nft_price": nft_price,
                 "type_found_param": type_found,
-                "data_path_var": file_cid
+                "metadata": metadata
             }
 
         )
@@ -157,26 +195,26 @@ async function add_donation(amount, type_found, name, file_cid){
 
 }
 
-var  type_found, name, short_info, image_array, video, type_pro, tags, nft_number;
+var type_found, name, short_info, image_array, video, type_pro, tags, nft_number;
 var amount;
 var nft_image_array = [];
 
-document.getElementById("create_btn").onclick = async () =>{
-    name =document.getElementById("pro_name").value
-    if (document.getElementById("inlineRadio1_1").checked){
+document.getElementById("create_btn").onclick = async() => {
+    name = document.getElementById("pro_name").value
+    if (document.getElementById("inlineRadio1_1").checked) {
         type_pro = true;
-    }else if(document.getElementById("inlineRadio1_2").checked){
+    } else if (document.getElementById("inlineRadio1_2").checked) {
         type_pro = false;
     }
-    if (document.getElementById("inlineRadio2_1").checked){
+    if (document.getElementById("inlineRadio2_1").checked) {
         type_found = true;
-    }else if(document.getElementById("inlineRadio2_2").checked){
+    } else if (document.getElementById("inlineRadio2_2").checked) {
         type_found = false;
     }
     short_info = document.getElementById("floatingShortInfo").value;
 
     video = document.getElementById("formVideoURL").value;
-    var image=document.getElementsByName("image");
+    var image = document.getElementsByName("image");
     image_array = await load_to_nft_storage(image);
     tags = document.getElementById("Project_tags").value;
     nft_number = document.getElementsByClassName("div_create_nft").length;
@@ -189,7 +227,7 @@ document.getElementById("create_btn").onclick = async () =>{
     amount = document.getElementById("Near_amount").value; //in NEAR
 
     amount = Number(amount);
-    if(nft_number>0) {
+    if (nft_number > 0) {
 
         let el = document.getElementsByName("nftName_create");
         let el1 = document.getElementsByName("img_nft_create");
@@ -205,40 +243,59 @@ document.getElementById("create_btn").onclick = async () =>{
         nft_image_array = await load_to_nft_storage(el1);
     }
 
-    var pro_data_create = {
-        "metadata": {
-            "name": name,
-            "short_info": short_info,
-            "image": image_array,
-            "video": video,
-            "type_pro": type_pro,
-            "project_tags": tags,
+    let pro_data_create = {
+        "name": name,
+        "short_info": short_info,
+        "image": image_array,
+        "video": video,
+        "type_pro": type_pro,
+        "project_tags": tags,
 
 
-        },
-        "nft_data": {
-            "nft_number": nft_number,
-            "name": nftName,
-            "img": nft_image_array,
-            "price": nftPrice, //in $NEAR
-            "short_info": NFT_Short_info,
-            "id_number": id_number
+    };
+    let nft_data = [];
+    if (nft_number > 0) {
+        for (let i = 0; i < nft_number; i++) {
+            nft_data.push({
+                "title": nftName[i], // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
+                "description": NFT_Short_info[i], // free-form description
+                "media": nft_image_array[i], // URL to associated media, preferably to decentralized, content-addressed storage
+                "media_hash": null, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
+                "copies": null, // number of copies of this set of metadata in existence when token was minted.
+                "issued_at": null, // When token was issued or minted, Unix epoch in milliseconds
+                "expires_at": null, // When token expires, Unix epoch in milliseconds
+                "starts_at": null, // When token starts being valid, Unix epoch in milliseconds
+                "updated_at": null, // When token was last updated, Unix epoch in milliseconds
+                "extra": null, // anything extra the NFT wants to store on-chain. Can be stringified JSON.
+                "reference": null, // URL to an off-chain JSON file with more info.
+                "reference_hash": null
+            });
         }
     }
+    /*,
+            "nft_data": {
+                "nft_number": nft_number,
+                "name": nftName,
+                "img": nft_image_array,
+                "price": nftPrice, //in $NEAR
+                "short_info": NFT_Short_info,
+                "id_number": id_number
+            }
+        }*/
 
-    const blob = new Blob([JSON.stringify(pro_data_create)], {type: 'application/json'});
-    const files = [new File([blob], name+'.json')];
-    let file_cid = await storeFiles(files);
+    /*    const blob = new Blob([JSON.stringify(pro_data_create)], { type: 'application/json' });
+        const files = [new File([blob], name + '.json')];
+        let file_cid = await storeFiles(files);*/
 
-    await add_donation(amount, type_found, name, file_cid);
+    await add_donation(amount, type_found, name, pro_data_create, nft_data, nftPrice);
 }
 
 
 
-async function load_to_nft_storage(image){
+async function load_to_nft_storage(image) {
     var img_array = [];
 
-    for (let i=0; i< image.length; i++){
+    for (let i = 0; i < image.length; i++) {
         var img = image[i].files;
         var cid = await storeFiles(img);
 
@@ -252,4 +309,3 @@ async function load_to_nft_storage(image){
 window.nearInitPromise = initContract()
     .then(doWork)
     .catch(console.error);
-
